e842600f2ebff5c06793755ba8de6155de9b70ae
GDDGprog <1543510611@qq.com>
2023-10-02 00:29:01 +0800

    添加了新的几个模块

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/test/java/com/yujian/XuechengPlusContentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusContentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
